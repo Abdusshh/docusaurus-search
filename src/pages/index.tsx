f02fefc866df8241362fec9f4fab99b80dc6c416
Abdusshh <abdullah.enes189@gmail.com>
2025-01-25 00:12:38 +0300

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,23 +5,31 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-
+import { ProductProvider, useProduct } from '@site/src/components/ProductContext';
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  const { hoveredProduct } = useProduct();
+  
+  const headerClassName = clsx('hero', styles.heroBanner, {
+    [styles.heroBannerRedis]: hoveredProduct === 'redis',
+    [styles.heroBannerVector]: hoveredProduct === 'vector',
+    [styles.heroBannerQStash]: hoveredProduct === 'qstash',
+  });
+
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={headerClassName}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
+        <Heading as="h1" className={styles.heroTitle}>
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className={styles.heroSubtitle}>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/vector/features/hybridindexes">
+            Using Upstash Vector Hybrid Indexes
           </Link>
         </div>
       </div>
@@ -32,13 +40,15 @@ function HomepageHeader() {
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+    <ProductProvider>
+      <Layout
+        title={`${siteConfig.title}`}
+        description="Serverless Data Platform">
+        <HomepageHeader />
+        <main>
+          <HomepageFeatures />
+        </main>
+      </Layout>
+    </ProductProvider>
   );
 }
